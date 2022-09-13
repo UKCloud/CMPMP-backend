c820dea37c55fa008a269735f8c660d023397f5a
Sonam Pal <spal@ukcloud.com>
2022-09-13 14:54:44 +0100

    changes to user.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,43 +4,44 @@ export const usersRouter = express.Router();
 
 /* GET users listing. */
 usersRouter.get('/', function(req:Request, res:Response, next:NextFunction) {
-  if (req.csrfToken()) {
-    axios
-      .post(
-        process.env.KEYCLOAK_INTROSPECTION_ENDPOINT,
-        qs.stringify({
-          client_id: process.env.CLIENT_ID,
-          client_secret: process.env.CLIENT_SECRET,
-          token: req.session.token
-        }))
-      .then((result) => {
-        db.User.findAll({
-          where:{
-            sid:result.data.sid
-          }
-        })
-        .then((searchresult) => {
-        if(searchresult=== 0){
-        //console.log(result.data);
-       db.User.create({
-          firstName:result.data.given_name,
-          lastName:result.data.family_name,
-          sid:result.data.sid
-        })
-      }
-      })
+  const user = { email: 'hello'};
+  // if (req.csrfToken()) {
+  //   axios
+  //     .post(
+  //       process.env.KEYCLOAK_INTROSPECTION_ENDPOINT,
+  //       qs.stringify({
+  //         client_id: process.env.CLIENT_ID,
+  //         client_secret: process.env.CLIENT_SECRET,
+  //         token: req.session.token
+  //       }))
+  //     .then((result) => {
+  //       db.User.findAll({
+  //         where:{
+  //           sid:result.data.sid
+  //         }
+  //       })
+  //       .then((searchresult) => {
+  //       if(searchresult=== 0){
+  //       //console.log(result.data);
+  //      db.User.create({
+  //         firstName:result.data.given_name,
+  //         lastName:result.data.family_name,
+  //         sid:result.data.sid
+  //       })
+  //     }
+  //     })
       
         
-        res.send(result.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+  //       res.send(result.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     })
   
-  } else {
-    console.log("no token");
-    res.send({});
-  }
+  // } else {
+  //   console.log("no token");
+  //   res.send({});
+  // }
 });
 
 
