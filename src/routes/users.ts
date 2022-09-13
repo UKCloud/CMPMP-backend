f8b5eba82207ca2be9558ecef9c1b52a08eb653e
Sonam Pal <spal@ukcloud.com>
2022-09-13 14:20:29 +0100

    added to user.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,46 @@
 import express, {Request, Response, NextFunction} from 'express';
+import axios from 'axios';
 export const usersRouter = express.Router();
 
 /* GET users listing. */
 usersRouter.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.send({user: {email: 'hello world'}});
+  if (req.csrfToken()) {
+    axios
+      .post(
+        process.env.KEYCLOAK_INTROSPECTION_ENDPOINT,
+        qs.stringify({
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          token: req.session.token
+        }))
+      .then((result) => {
+        db.User.findAll({
+          where:{
+            sid:result.data.sid
+          }
+        })
+        .then((searchresult) => {
+        if(searchresult=== 0){
+        //console.log(result.data);
+       db.User.create({
+          firstName:result.data.given_name,
+          lastName:result.data.family_name,
+          sid:result.data.sid
+        })
+      }
+      })
+      
+        
+        res.send(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  
+  } else {
+    console.log("no token");
+    res.send({});
+  }
 });
 
 
