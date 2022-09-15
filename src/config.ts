69f5a0c0e64736908f21e7655cfcbba82a7d7836
Sonam Pal <spal@ukcloud.com>
2022-09-15 09:13:49 +0100

    Changes to logout route

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,11 @@ dotenv.config();
 
 export const config = {
     host: process.env.SESSION_HOST,
-    port: Number(process.env.SESSION_PORT) || 3000,
+    port: Number(process.env.SESSION_PORT) || 9000,
     user: process.env.SESSION_USER,
     password: process.env.SESSION_PASSWORD,
     database: process.env.SESSION_DATABASE,
     secret: String(process.env.SESSION_SECRET),
-    nodeEnv: process.env.NODE_ENV
+    nodeEnv: process.env.NODE_ENV,
+    logoutRedirectURI: process.env.LOGOUT_REDIRECT_URI
 }
--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -1,11 +1,14 @@
 import express, {Request, Response, NextFunction} from 'express';
 import qs from 'query-string';
 export const logoutRouter = express.Router();
+import { config } from "../config.js";
 
 logoutRouter.get('/', function(req:Request, res:Response, next:NextFunction) {
-  req.csrfToken();
+  req.session.destroy(() => {
+    res.json({message:"Logged Out"})
+});
   const data = qs.stringify({
-    post_logout_redirect_uri: "http://localhost:5173",
+    post_logout_redirect_uri: config.logoutRedirectURI ,
     client_id: process.env.CLIENT_ID
   })
   res.redirect(`${process.env.KEYCLOAK_LOGOUT_ENDPOINT}?${data}`)
