77f39f83ee1ddce8d590f22ef4aa49d50f48c481
Kieran Gates <kgates@ukcloud.com>
2022-09-26 15:19:59 +0100

    chore:remove comment in dashboard

--- src/models/dashboard.ts
+++ src/models/dashboard.ts
@@ -1,28 +1,6 @@
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../database";
 
-// export const Dashboard = sequelize.define(
-//   "dashboards",
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     // Just a 'dumb' field for now. In theory though an entire
-//     // dashboard could be saved, assuming the STRING limit isn't reached
-//     data: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//   }
-// );
-
 export class Dashboard extends Model {}
 
 Dashboard.init({
@@ -36,7 +14,7 @@ Dashboard.init({
       type: DataTypes.STRING,
       allowNull: false,
     },
-    // Just a 'dumb' field for now. In theory though an entire
+    // Just a basic field for now. In theory though an entire
     // dashboard could be saved, assuming the STRING limit isn't reached
     data: {
       type: DataTypes.STRING,
