2b8fea102c6b6bab81c17cfd0b90f67067672501
Sonam Pal <spal@ukcloud.com>
2022-09-28 11:39:35 +0100

    changes

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -1,54 +1,48 @@
 // import express, { Request, Response } from 'express';
-// import MySQLStore from 'express-mysql-session';
+// import myStore from 'express-mysql-session';
 // import { app } from '../app';
 // import { Dashboard } from '../models/dashboard';
 // export const dashboardRouter = express.Router();
 
+// dashboardRouter.post("/", function (req: Request, res: Response) {
+//     const createDashboard = {
+//         // id : req.body.name,
+//         // name : req.body.name,
+//         // data : req.body.data,
+       
 
-// function createDashboard(req : Request, res: Response){
-//     const post = {
-//         id : req.body.id,
-//         name : req.body.name,
-//         data : req.body.data,
-//         createdAt : req.body.createdAt,
-//         updatedAt : req.body.updatedAt
+//         id: 2,
+//         name: "Second test ",
+//         data: "Does this work",
+        
 //     }
+//     Dashboard.create(createDashboard).then(result => {
+//         res.status(201).json({
+//             message: "succesfully created",
+//             post: result
+//         });
+
+//     }).catch(error => {
+//         res.status(500).json({
+//             message: "unsuccessful",
+//             error: error
+//         })
+//     });
 // }
+// );
 
 
-// dashboardRouter.post('/',(req,res)=>{
-
-
-//     const dashboard = {
-//         id : req.body.id,
-//         name : req.body.name,
-//         data : req.body.data,
-//         createdAt : req.body.createdAt,
-//         updatedAt : req.body.updatedAt
-//     }
-//     MySQLStore.query('INSERT INTO dashboards VALUES ()', [id,name,data,createdAt,updatedAt],(err,result)=>{
-//         if(err){
-//             console.log(err)
-//         }else{
-//             res.send("Created")
-//         }
-//     })
-// }
-// )
 // dashboardRouter.get('/:id',  function (req: Request, res: Response) {
-//     Dashboard.findAll({ where: { id: req.params.id } }).then(Dashboard => res.json(Dashboard));
+//     Dashboard.findByPk(req.params.id).then(Dashboard => res.json(Dashboard));
 // });
 
 
 
-//   dashboardRouter.delete('/:id', function (req: Request, res: Response) {
-//     Dashboard.findByPk(req.params.id).then(function(dashboard) {
-//         dashboard.destroy();
-//       }).then((dashboard) => {
-//         res.sendStatus(200);
-//       });
-// });
+// //   dashboardRouter.delete('/:id', function (req: Request, res: Response) {
+// //     Dashboard.findByPk(req.params.id).then(function(dashboard) {
+// //         dashboard.destroy();
+// //       }).then((dashboard) => {
+// //         res.sendStatus(200);
+// //       });
+// // });
 
-// // app.post('/',function(req :  Request,res: Response)  {
-// //     console.log("sonam")
-// // })
