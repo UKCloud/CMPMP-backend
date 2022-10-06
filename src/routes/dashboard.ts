74d9f0b558b7ea3bdce06931b880485297a02f75
alexiswillowp <alexiswillowpaterson@gmail.com>
2022-10-06 16:45:23 +0100

    added swagger documentation to dashboard API

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -4,41 +4,11 @@ import { isLoggedIn } from '../middleware/authorisation';
 import { Dashboard } from '../models/dashboard';
 export const dashboardRouter = express.Router();
 
-dashboardRouter.post("/",isLoggedIn, function (req: Request, res: Response) {
-    const createDashboard = {
-        name: req.body.name,
-        data: req.body.data,
-    }
-    console.log(req.body.id)
-    Dashboard.findByPk(req.body.id).then(dashboard => {
-        if (dashboard) {
-            dashboard.update(createDashboard).then(result => {
-                // Updates dashboards 
-                res.status(200).json({
-                    message: "succesfully updated",
-                    createDashboard: result
-                });
-            }).catch(error => {
-                res.status(500).json({
-                    message: "unsuccessfully updated",
-                    error: error
-                })
-            })
-        } else {
-            // Create dashboards
-            Dashboard.create(createDashboard).then(result => {
-                res.status(200).json({
-                    message: "succesfully created",
-                    createDashboard: result
-                });
-            }).catch(error => {
-                res.status(500).json({
-                    message: "unsuccessfully created",
-                    error: error
-                })
-            });
-        }
-    })
+dashboardRouter.post("/",isLoggedIn, async function (req: Request, res: Response) {
+    const controller = new DashboardController();
+    const response = await controller.createDashboards(req.body);
+    res.status(response[0]);
+    return res.json(response[1]);
     // }
 })
 
@@ -51,18 +21,22 @@ dashboardRouter.get('/', isLoggedIn, async function (req: Request, res: Response
 });
 
 // GET request for retrieving dashboard by ID
-dashboardRouter.get('/:id',isLoggedIn, function (req: Request, res: Response) {
-    Dashboard.findByPk(req.params.id).then(Dashboard => res.json(Dashboard));
-    console.log(req.params.id);
+dashboardRouter.get('/:id',isLoggedIn, async function (req: Request, res: Response) {
+    const controller = new DashboardController();
+    const response = await controller.findDashboardById(req.params.id);
+    return res.json(response);
 });
 
 // DELETE request to delete a dashboard
-dashboardRouter.delete('/:id',isLoggedIn, function (req: Request, res: Response) {
-    Dashboard.findByPk(req.params.id).then(function (createDashboard) {
-        createDashboard?.destroy();
-    }).then((createDashboard) => {
-        res.sendStatus(200);
-    });
+dashboardRouter.delete('/:id',isLoggedIn, async function (req: Request, res: Response) {
+    const controller = new DashboardController();
+    const response = await controller.deleteDashboard(req.params.id);
+    return res.json(response);
+    // Dashboard.findByPk(req.params.id).then(function (createDashboard) {
+    //     createDashboard?.destroy();
+    // }).then((createDashboard) => {
+    //     res.sendStatus(200);
+    // });
 });
 
 
